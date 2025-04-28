e46bea6017f40a88e66cc99c2d2fc88d5f147f94
alex <alexgim961101@gmail.com>
2025-04-29 01:49:00 +0900

    간단한 영화 정보 CRUD 구현

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api', {
+    exclude: [
+      { path: 'health', method: RequestMethod.GET },
+      { path: 'docs', method: RequestMethod.GET },
+    ],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
